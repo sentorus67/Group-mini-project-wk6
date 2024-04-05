f03c2e2d9ed34deb4d63a8040a3efe6eb22bd1e6
Malcolm Franklin <mac.mac5@yahoo.com>
2024-04-04 20:53:46 -0600

    actually added the submit/search button

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const requestUrl = 'https://www.loc.gov/search/?fo=json';
+const searchInput = document.getElementById('search');
+
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    const search = searchInput.value();
+
+    if (!search) {
+        alert("Please enter something to search.");
+        return; // Stop further execution if the input is empty
+    }
+
+    // Continue with the search process
+    getInfoFromSearch(search);
+
+    searchInput.value = ""; // Clear the input field after search
+}
+
+
 
 function getApi() {
     fetch(requestUrl)
@@ -8,4 +27,6 @@ function getApi() {
         .then(function (data){
             consolog.log(data);
         })
-}  
+
+
+}
